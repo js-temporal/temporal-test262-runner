0bb45ddb3acc330cec92cc8d9b548d9d5a1b0385
James Wright <jameswr@google.com>
2023-01-18 11:32:13 -0800

    Return a failing result if any tests unexpectedly pass. (#6)
    * Use `npm link` to test the HEAD runner.
    * Make `npm test` fail if any tests unexpectedly pass.

--- test/test.sh
+++ test/test.sh
@@ -6,6 +6,6 @@ cd test
 rm -rf temporal-polyfill
 git clone --recurse-submodules https://github.com/js-temporal/temporal-polyfill.git
 cd temporal-polyfill
-npm i
-TEST262=1 npm run build
-node --loader ../resolve-test.mjs ../runtest262.mjs
+npm i --ignore-scripts --no-audit
+npm link ../../ --ignore-scripts --no-audit
+npm run test262
