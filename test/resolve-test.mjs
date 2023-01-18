44d2a540bbccf635f4d10bea8dc39acaf35afd73
James Wright <jameswr@google.com>
2023-01-17 16:10:26 -0800

    Use NPM Link to test the HEAD runner.

--- test/resolve-test.mjs
+++
@@ -1,8 +0,0 @@
-import fs from 'fs';
-const PKG = JSON.parse(fs.readFileSync('../../package.json', { encoding: 'utf-8' }));
-export function resolve(specifier, parent, defaultResolve) {
-  if (specifier === PKG.name) {
-    specifier = new URL('../index.mjs', import.meta.url).toString();
-  }
-  return defaultResolve(specifier, parent);
-}
